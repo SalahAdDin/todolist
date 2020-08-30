2e282c3b73776d9114a02a2722506e9cfe408973
José L. S. A <alagunasalahaddin@live.com>
2020-08-30 17:18:27 +0300

    Adding button to complete all tasks.

--- src/components/form.js
+++ src/components/form.js
@@ -2,24 +2,43 @@ import React from "react"
 import PropTypes from "prop-types"
 import id from "shortid"
 import { useFormik } from "formik"
+import { Button } from "primereact/button"
 import { InputText } from "primereact/inputtext"
 
 const Form = ({ oldTasksList, setTasks }) => {
+  const [allActive, setAllActive] = React.useState(false)
   const {
     values: { task },
     handleChange,
     handleSubmit,
   } = useFormik({
     initialValues: { task: "" },
-    onSubmit: ({ task }) =>
-      setTasks([...oldTasksList, { id: id.generate(), task, active: true }]),
+    onSubmit: ({ task }, { resetForm }) => {
+      setTasks([...oldTasksList, { id: id.generate(), task, active: true }])
+      resetForm()
+    },
   })
+
+  React.useEffect(() => {
+    const newList = oldTasksList.map((task) => {
+      return { ...task, active: allActive }
+    })
+    setTasks(newList)
+  }, [allActive])
+
+  const handleOnClick = () => {
+    setAllActive(!allActive)
+  }
 
   return (
     <div className="p-fluid">
-      <form className="p-field" onSubmit={handleSubmit}>
-        <span className="p-input-icon-left">
-          <i className="pi pi-angle-down" />
+      <span className="p-input-icon-left">
+        <Button
+          icon="pi pi-angle-down"
+          className="p-button-rounded p-button-success p-button-text"
+          onClick={handleOnClick}
+        />
+        <form className="p-field" onSubmit={handleSubmit}>
           <InputText
             id="task"
             name="task"
@@ -28,8 +47,8 @@ const Form = ({ oldTasksList, setTasks }) => {
             onChange={handleChange}
             value={task}
           />
-        </span>
-      </form>
+        </form>
+      </span>
     </div>
   )
 }
